fcff11261feb85432045664509752ce9ea664be8
Ro6afF <dimo_chanev@windowslive.com>
2016-05-29 14:46:53 +0300

    Random commit

--- Algo/Telerik/Algo/27-05-2016/bad_cat.cpp
+++ Algo/Telerik/Algo/27-05-2016/bad_cat.cpp
@@ -7,52 +7,52 @@ std::set<int> asdf;
 
 int main()
 {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	int n;
-	std::cin >> n;
-	tatko.resize(10);
-	std::string asd;
-	for(int i = 1; i <= 9; i++)
-	{
-		tatko[i] = i;
-	}
-	for(int i = 0; i < n; i++)
-	{
-		std::string s2, s3;
-		int s1, s4;
-		std::cin >> s1 >> s2 >> s3 >> s4;
-		asdf.insert(s1);
-		asdf.insert(s4);
-		if(s3 == "before")
-		{
-			tatko[s4] = s1;	
-		}
-		else
-		{
-			tatko[s1] = s4;
-		}
-	}
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int n;
+    std::cin >> n;
+    tatko.resize(10);
+    std::string asd;
+    for(int i = 1; i <= 9; i++)
+    {
+        tatko[i] = i;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        std::string s2, s3;
+        int s1, s4;
+        std::cin >> s1 >> s2 >> s3 >> s4;
+        asdf.insert(s1);
+        asdf.insert(s4);
+        if(s3 == "before")
+        {
+            tatko[s4] = s1;
+        }
+        else
+        {
+            tatko[s1] = s4;
+        }
+    }
 
-	for(auto x: asdf)
-	{
-		asd+=x + '0';
-	}
+    for(auto x: asdf)
+    {
+        asd+=x + '0';
+    }
 
-	for(int i = 0; i < asd.size(); i++)
-	{
-		if(tatko[asd[i] - '0'] != asd[i] - '0')
-		{
-			for(int j = i; j < asd.size(); j++)
-			{
-				if(asd[j] == tatko[asd[i]-'0']+'0')
-				{
-					std::swap(asd[i], asd[j]);
-					break;
-				}
-			}
-		}
-	}
-	std::cout<< asd<<std::endl;
-	return 0;
+    for(int i = 0; i < asd.size(); i++)
+    {
+        if(tatko[asd[i] - '0'] != asd[i] - '0')
+        {
+            for(int j = i; j < asd.size(); j++)
+            {
+                if(asd[j] == tatko[asd[i]-'0']+'0')
+                {
+                    std::swap(asd[i], asd[j]);
+                    break;
+                }
+            }
+        }
+    }
+    std::cout<< asd<<std::endl;
+    return 0;
 }
---
+++ Algo/Telerik/Kids/29-05-2016/AI_code.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// You can create any global and local variables but make them UNIQUE
+int how_many_balls_0(int min_ball,int max_ball,int current_number_balls)
+{
+    int res;
+
+    /// Your code for AI must be here!
+    std::cin>>res;
+    return res;
+}
+
